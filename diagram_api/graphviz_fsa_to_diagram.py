5d11e2e5c10fda87a1931be28d1089aa660dd020
Huryel Souto Costa <huryel.souto@gmail.com>
2023-11-01 16:06:46 -0300

    funcionando quase todas definições regulares

--- diagram_api/graphviz_fsa_to_diagram.py
+++ diagram_api/graphviz_fsa_to_diagram.py
@@ -1,3 +1,4 @@
+import re
 import json
 from diagram_api.graphviz_fsa_to_diagram_model import convert_graphviz_fsa_to_driagram_model
 
@@ -34,8 +35,76 @@ def graphviz_fsa_to_diagram(dir):
               "finais": finais,
               "transicoes": transicoes,
               "lookaheads": list(look_aheads)})
+    
+    # print(d['transicoes'])
+    # ['0', '69', 'acdfghjklmnoqsvxyzACDFGHJKLMNOQSVXYZ']
+    # ['0', 'dig', 'digito']
+    newTransicoes = []
+    ascii_characters = ''.join([chr(i) for i in range(32, 127)])
+    print(ascii_characters)
 
+    for transicao in d['transicoes']: 
+
+        if(len(transicao[2]) > 1):
+            if transicao[2][0] != '^': # digito (CERTOOOO)
+                stringChars = d['definicoes_regulares'][transicao[2]] if(transicao[2] in d['definicoes_regulares'].keys()) else transicao[2]
+                for character in stringChars:
+                        newTransicoes.append([transicao[0], transicao[1], character])
+            else: 
+                definicaoRegular = transicao[2][1:]
+                
+                if(len(definicaoRegular) == 1): # ^p (CERTOOOO)
+                    str_form = f'[^{definicaoRegular}]'
+                    try:
+                        ascii_characters_exc = re.findall(str_form, ascii_characters)
+                    except:
+                        print('AAAAAAA')
+                        str_form = f'[^\{definicaoRegular}]'
+                        ascii_characters_exc = re.findall(str_form, ascii_characters)
+                        # print(str_form)
+                        # print(ascii_characters)
+
+                    for character in ascii_characters_exc:               
+                        newTransicoes.append([transicao[0], transicao[1], character])
+                else: # ^digito , ^digito_ 
+                    ascii_intersection = ''.join([chr(i) for i in range(32, 127)])
+
+                    for definicao in d['definicoes_regulares'].keys(): # digitoletra
+                        padrao_def = definicao
+                        correspondencia_def = re.search(padrao_def, definicaoRegular)
+                        
+                        if correspondencia_def:
+                            for character in ascii_intersection:
+                                if character not in d['definicoes_regulares'][definicao]:
+                                    ascii_intersection.replace(character, '')
+                            
+                            definicaoRegular = re.findall(definicao, definicaoRegular)
+                            definicaoRegular = ''.join(definicaoRegular)
+                    
+                    str_form = f'[^{definicaoRegular}]' # sobra _ em digito_
+                    try:
+                        ascii_characters_exc = re.findall(str_form, ascii_intersection)
+                    except:
+                        str_form = f'[^\{definicaoRegular}]'
+                        ascii_characters_exc = re.findall(str_form, ascii_intersection)
+                        
+                    for character in ascii_characters_exc:               
+                        newTransicoes.append([transicao[0], transicao[1], character])
+                    
+        else: # p CERTOOOO
+            newTransicoes.append([transicao[0], transicao[1], transicao[2]])
+
+        
+        print(transicao)
+
+    print('AAAAAAAA')
+    for t in newTransicoes:
+        if (t[0] == '103' and t[1] == '103'):
+            print(t)
+    # print(newTransicoes)
     d = json.dumps(d)
+
+
 
     with open("diagram_api/diagrama_final.json", "w") as f:
         f.write(d)
