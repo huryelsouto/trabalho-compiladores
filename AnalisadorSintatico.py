0a39f00391cec6ef9c2bbad508af23f92d768ce5
ph-marra <ph_marra@hotmail.com>
2023-11-04 17:23:18 -0300

    sisntatico

--- AnalisadorSintatico.py
+++ AnalisadorSintatico.py
@@ -30,28 +30,35 @@ class AnalisadorSintatico():
     def proc(self, Xi):
         return {'S': self.S,
                 'bloco': self.bloco,
-                'dec_vars': self.dec_vars,
+                'declare_vars': self.declare_vars,
                 'tipo': self.tipo,
                 'seq_cmd': self.seq_cmd,
                 'comment': self.comment,
                 'cmd': self.cmd,
                 'exp_arit': self.exp_arit,
+                'exp_arit_aux': self.exp_arit_aux,
                 'exp_arit_termo': self.exp_arit_termo,
+                'exp_arit_termo_aux': self.exp_arit_termo_aux,
                 'exp_arit_elev': self.exp_arit_elev,
+                'exp_arit_elev_aux': self.exp_arit_elev_aux,
                 'exp_arit_fator': self.exp_arit_fator,
                 'exp_rel': self.exp_rel,
+                'exp_rel_aux': self.exp_rel_aux,
                 'exp_rel_and': self.exp_rel_and,
+                'exp_rel_and_aux': self.exp_rel_and_aux,
                 'exp_rel_eq': self.exp_rel_eq,
+                'exp_rel_eq_aux': self.exp_rel_eq_aux,
                 'exp_rel_comp': self.exp_rel_comp,
+                'exp_rel_comp_aux': self.exp_rel_comp_aux,
                 'exp_rel_fator': self.exp_rel_fator,
-                'constant_fator': self.constant_fator,
                 'constant_char': self.constant_char,
+                'constant_int': self.constant_int,
                 'constant_float': self.constant_float
                 }[Xi]
 
 
     def S(self):
-        if self.proxToken == 'function':
+        if self.proxToken == 'program':
             self.proxToken = self.lex.__prox_token__()
 
             if self.proxToken == 'id':
@@ -76,10 +83,159 @@ class AnalisadorSintatico():
             self.proc(self.proxToken)()
 
         else:
-            self.trata_erro(self, 'function')
+            self.trata_erro(self, 'program')
             
 
     def bloco(self):
+        pass
+
+
+    def declare_vars(self):
+        self.proc('tipo')()
+
+        if self.proxToken == ':':
+            self.proxToken = self.lex.__prox_token__()
+            self.proc('lista_ids')()
+
+            if self.proxToken == ';':
+                self.proxToken = self.lex.__prox_token__()
+
+            else:
+                self.trata_erro(self, ';')
+
+        else:
+            self.trata_erro(self, ':')
+
+
+    def tipo(self):
+        if self.proxToken in ['int', 'char', 'float']:
+            self.proxToken = self.lex.__prox_token__()
+        
+        else:
+            self.trata_erro(self, str(['int', 'char', 'float']))
+
+
+    # tá meio errado conferir
+    def lista_ids(self):
+        if self.proxToken == 'id':
+            self.proxToken = self.lex.__prox_token__()
+
+            if self.proxToken == ',':
+                self.proxToken = self.lex.__prox_token__()
+                self.proc('lista_ids')()
+
+            else:
+                self.trata_erro(self, ',')
+
+        else:
+            self.trata_erro(self, 'id')
+
+
+    def comment(self):
+        if self.proxToken == '{':
+            self.proxToken = self.lex.__prox_token__()
+
+            while self.proxToken != '}':
+                self.proxToken = self.lex.__prox_token__()
+
+            if self.proxToken == '}':
+                self.proxToken = self.lex.__prox_token__()
+
+            else:
+                self.trata_erro(self, '}')
+
+        else:
+            self.trata_erro(self, '{')
+
+    
+    def seq_cmd(self):
+        pass
+
+
+    def cmd(self):
+        pass
+
+
+    def exp_arit(self):
+        self.proc('exp_arit_termo')()
+        self.proc('exp_arit_aux')()
+
+    
+    def exp_arit_aux(self):
+        pass
+
+
+    def exp_arit_termo(self):
+        self.proc('exp_arit_elev')()
+        self.proc('exp_arit_termo_aux')()
+
+
+    def exp_arit_termo_aux(self):
+        pass
+
+
+    def exp_arit_elev(self):
+        self.proc('exp_arit_fator')()
+        self.proc('exp_arit_elev_aux')()
+
+
+    def exp_arit_elev_aux(self):
+        pass
+
+
+    def exp_arit_fator(self):
+        pass
+
+
+    def exp_rel(self):
+        self.proc('exp_rel_and')()
+        self.proc('exp_rel_aux')()
+
+
+    def exp_rel_aux(self):
+        pass
+
+
+    def exp_rel_and(self):
+        self.proc('exp_rel_eq')()
+        self.proc('exp_rel_and_aux')()
+
+
+    def exp_rel_and_aux(self):
+        pass
+
+
+    def exp_rel_eq(self):
+        self.proc('exp_rel_comp')()
+        self.proc('exp_rel_eq_aux')()
+
+
+    def exp_rel_eq_aux(self):
+        pass
+
+
+    def exp_rel_comp(self):
+        self.proc('exp_rel_fator')()
+        self.proc('exp_rel_comp_aux')()
+
+    
+    def exp_rel_comp_aux(self):
+        pass
+
+
+    def exp_rel_fator(self):
+        pass
+
+
+    def constant_char(self):
+        pass
+
+
+    def constant_int(self):
+        pass
+
+
+    def constant_float(self):
         pass
 
 
