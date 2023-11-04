2303cfe5f32cb4298992d3ef24784496d3f8cead
Huryel Souto Costa <huryel.souto@gmail.com>
2023-11-04 18:48:09 -0300

    merging

--- teste.py
+++ teste.py
@@ -7,10 +7,9 @@ graphviz_fsa_to_diagram('diagram_api/graphviz_fsa.txt')
 tabela_simbolos = TabelaDeSimbolos()
 lex = AnalisadorLexico("diagram_api/diagrama_final.json", "while.txt", tabela_simbolos)
 
-<<<<<<< Updated upstream
-tokens = lex.run()
+# tokens = lex.run()
+# print(tabela_simbolos)
 
-=======
 #G = Gramatica(V, T, P, S)
 G = Gramatica.from_json("gramatica.json")
 
@@ -18,33 +17,11 @@ G = Gramatica.from_json("gramatica.json")
 #print(G.__pApcl__(pApc))
 
 print(G)
->>>>>>> Stashed changes
 
 # for token in tokens:
 #     print(token)
 
-<<<<<<< Updated upstream
-print(tabela_simbolos)
-
-# V = ["E", "E\'", "T", "T\'", "F"]
-# T = ["+", "", "*", "(", ")", "id"]
-# P = [["E", "T E\'"], ["E\'", "+ T E\'"], ["E\'", ""],
-#      ["T", "F T\'"], ["T\'", "* F T\'"], ["T\'", ""],
-#      ["F", "( E )"], ["F","id"]]
-# S = "E"
-
-# G = Gramatica(V, T, P, S)
-# #G = Gramatica.from_json("gramatica.json")
-
-# pApc = G.__encontra_pApc__()
-# print(G.__pApcl__(pApc))
-
-# print("\n----------------\n")
-
-# G.fatorar_gramatica()
-=======
 #G.fatorar_gramatica()
->>>>>>> Stashed changes
 
 for v in G.V:
     print(f"FIRST({v})={G.first(v)}")
